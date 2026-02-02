33c3dafdfa5a006574b70152368878e7e6de4f10
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-02-02 17:38:25 +0200

    Create Module classes / extension methods to handle each modules dependencies

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,13 +1,14 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
+
 var app = builder.Build();
-
-app.UseHttpsRedirection();
-
-app.MapGet("/", () =>
-{
-    return "Hello world";
-});
 
 app.Run();
 
