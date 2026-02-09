15b5e592b6448a6e09b7aa6e69966c43511da128
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-02-09 17:53:13 +0200

    work on ddd base objects and interfaces

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.DDD;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{ }
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
