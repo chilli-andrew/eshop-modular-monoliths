e195f6dd14b1d9b6a383719d9530d29dd9ceaf20
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-02-24 18:01:51 +0200

    Work on seed data and AuditableEntityInterceptor

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Shared.Data;
 
 namespace Catalog;
 
@@ -19,6 +18,8 @@ public static class CatalogModule
         var connectionString = configuration.GetConnectionString("Database");
         services.AddDbContext<CatalogDbContext>(options =>
             options.UseNpgsql(connectionString));
+        
+        services.AddScoped<IDataSeeder, CatalogDataSeeder>();
         
         return services;
     }
--- src/Shared/Shared/Data/Extensions.cs
+++ src/Shared/Shared/Data/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Seed;
 
 namespace Shared.Data;
 
@@ -10,14 +11,26 @@ public static class Extensions
         where TContext : DbContext
     {
         MigrateDatabaseAsync<TContext>(app.ApplicationServices).GetAwaiter().GetResult();
+        SeedDataAsync(app.ApplicationServices).GetAwaiter().GetResult();
         return app;
     }
-    
+
     private static async Task MigrateDatabaseAsync<TContext>(IServiceProvider services)
         where TContext : DbContext
     {
         using var scope = services.CreateScope();
         var context = scope.ServiceProvider.GetRequiredService<TContext>();
         await context.Database.MigrateAsync();
+    }
+
+    private static async Task SeedDataAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var seeders = scope.ServiceProvider.GetServices<IDataSeeder>();
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAllAsync();
+        }
+
     }
 }
