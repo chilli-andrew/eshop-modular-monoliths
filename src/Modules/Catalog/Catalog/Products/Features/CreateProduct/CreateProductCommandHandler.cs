4491f9b793a5456c1032d1c40568ed21363252ee
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-03-05 18:00:33 +0200

    Work on CQRS abstractions

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler: IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
