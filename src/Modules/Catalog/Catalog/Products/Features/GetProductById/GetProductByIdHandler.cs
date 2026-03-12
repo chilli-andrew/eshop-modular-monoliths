04cf7e23c77849eb81765699ad57b680540e95a8
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-03-12 18:09:10 +0200

    work on query handlers

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.Products.Features.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(ProductDto Product);
+
+public class GetProductByIdHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await dbContext.Products
+            .AsNoTracking()
+            .SingleOrDefaultAsync(x => x.Id == query.Id, cancellationToken);
+        if (product == null) throw new Exception($"Product not found: {query.Id}");
+        var productDto = product.Adapt<ProductDto>();
+        return new GetProductByIdResult(productDto);
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.Products.Features.GetProducts;
+
+public record GetProductsQuery : IQuery<GetProductsResult>;
+
+// ReSharper disable once NotAccessedPositionalProperty.Global
+public record GetProductsResult(IEnumerable<ProductDto> Products);
+
+public class GetProductsHandler(CatalogDbContext dbContext) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .OrderBy(x => x.Name)
+            .ToListAsync(cancellationToken);
+        var productDtos = products.Adapt<List<ProductDto>>();
+        return new GetProductsResult(productDtos);
+    }
+}
