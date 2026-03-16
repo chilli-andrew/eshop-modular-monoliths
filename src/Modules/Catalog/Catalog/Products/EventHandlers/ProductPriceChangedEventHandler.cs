975072a880b64a2df86d92af8da37518fdb320ea
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-03-16 18:06:10 +0200

    Work on EventHandlers and Endpoints

---
+++ src/Modules/Catalog/Catalog/Products/EventHandlers/ProductPriceChangedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Products.EventHandlers;
+
+public class ProductPriceChangedEventHandler(ILogger<ProductPriceChangedEventHandler> logger) : INotificationHandler<ProductPriceChangedEvent>
+{
+    public Task Handle(ProductPriceChangedEvent notification, CancellationToken cancellationToken)
+    {
+        //TODO: publish product price changed integration event to update basket prices
+        logger.LogInformation("Domain event handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
