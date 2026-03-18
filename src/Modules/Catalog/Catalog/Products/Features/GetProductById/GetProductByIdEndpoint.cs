b8749633d73d3e480616115f5330a7fb5b585921
Andrew Russell <Andrew.Russell@chillisoft.co.za>
2026-03-18 17:42:19 +0200

    Work on implementation of endpoints for product

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.Products.Features.GetProductById;
+
+public record GetProductByIdResponse(ProductDto Product);
+
+public class GetProductByIdEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductByIdResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetProductById Summary")
+            .WithDescription("GetProductById Description");
+    }
+}
---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.Products.Features.GetProducts;
+
+public record GetProductsResponse(IEnumerable<ProductDto> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+            Results.Ok(response);
+        })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetProducts Summary")
+            .WithDescription("GetProducts Description");
+    }
+}
